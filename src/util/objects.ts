2dc467cf5338a7330db30455a67ce82cce5cd19d
Stephen C. Harris <steveofar@hotmail.com>
2021-05-23 14:48:12 -0500

    Remove unneeded log statement.

--- src/util/objects.ts
+++ src/util/objects.ts
@@ -4,7 +4,6 @@ export function deepEquals(o1: any, o2: any): boolean
   if ( typeof o1 !== 'object' || o1 == null )
     return o1 === o2;
 
-  console.log(o1);
   const keys1 = Object.keys(o1);
   const keys2 = Object.keys(o2);
 
