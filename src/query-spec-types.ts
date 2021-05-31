a4c6ffcae5725cde0cb0345a08cbccb33309bfdd
Stephen C. Harris <steveofar@hotmail.com>
2021-05-31 09:07:03 -0500

    Separate query spec types into separate module.

---
+++ src/query-spec-types.ts
@@ -0,0 +1,89 @@
+export interface QueryGroupSpec
+{
+  defaultSchema?: string;
+  propertyNameDefault?: PropertyNameDefault;
+  generateUnqualifiedNamesForSchemas: string[];
+  querySpecs: QuerySpec[];
+}
+
+export interface QuerySpec
+{
+  queryName: string;
+  tableJson: TableJsonSpec;
+  resultRepresentations?: ResultRepr[];
+  generateResultTypes?: boolean;
+  generateSource?: boolean;
+  propertyNameDefault?: PropertyNameDefault;
+  orderBy?: string;
+  forUpdate?: boolean;
+  typesFileHeader?: string;
+}
+
+export interface TableJsonSpec
+{
+  table: string;
+  fieldExpressions?: (string | TableFieldExpr)[];
+  parentTables?: ParentSpec[];
+  childTables?: ChildSpec[];
+  recordCondition?: RecordCondition;
+}
+
+export interface TableFieldExpr
+{
+  field?: string;
+  expression?: string;
+  jsonProperty?: string; // Required if value is not a simple field name.
+  fieldTypeInGeneratedSource?: string | {[srcLang: string]: string};
+  withTableAliasAs?: string; // Table alias escape sequence which may be used in value (default '$$').
+}
+
+export interface ParentSpec
+{
+  referenceName?: string | undefined;
+  customJoinCondition?: CustomJoinCondition;
+  tableJson: TableJsonSpec;
+  viaForeignKeyFields?: string[];
+}
+
+export interface ReferencedParentSpec extends ParentSpec
+{
+  referenceName: string;
+}
+
+export interface InlineParentSpec extends ParentSpec
+{
+  referenceName: undefined;
+}
+
+export interface ChildSpec
+{
+  collectionName: string;
+  tableJson: TableJsonSpec;
+  foreignKeyFields?: string[];
+  customJoinCondition?: CustomJoinCondition;
+  filter?: string;
+  unwrap?: boolean;
+  orderBy?: string;
+}
+
+export interface CustomJoinCondition
+{
+  equatedFields: FieldPair[];
+}
+
+export interface FieldPair
+{
+  childField: string;
+  parentPrimaryKeyField: string;
+}
+
+export interface RecordCondition
+{
+  sql: string;
+  paramNames?: string[];
+  withTableAliasAs?: string;
+}
+
+export type PropertyNameDefault = "AS_IN_DB" | "CAMELCASE";
+
+export type ResultRepr = "MULTI_COLUMN_ROWS" | "JSON_OBJECT_ROWS" | "JSON_ARRAY_ROW";
--- src/query-specs.ts
+++ src/query-specs.ts
@@ -1,116 +1,6 @@
-
-export interface QueryGroupSpec
-{
-  defaultSchema?: string;
-  propertyNameDefault?: PropertyNameDefault;
-  generateUnqualifiedNamesForSchemas: string[];
-  querySpecs: QuerySpec[];
-}
-
-export interface QuerySpec
-{
-  queryName: string;
-  tableJson: TableJsonSpec;
-  resultRepresentations?: ResultRepr[];
-  generateResultTypes?: boolean;
-  generateSource?: boolean;
-  propertyNameDefault?: PropertyNameDefault;
-  orderBy?: string;
-  forUpdate?: boolean;
-  typesFileHeader?: string;
-}
-
-export interface TableJsonSpec
-{
-  table: string;
-  fieldExpressions?: (string | TableFieldExpr)[];
-  parentTables?: ParentSpec[];
-  childTables?: ChildSpec[];
-  recordCondition?: RecordCondition;
-}
-
-export interface TableFieldExpr
-{
-  field?: string;
-  expression?: string;
-  jsonProperty?: string; // Required if value is not a simple field name.
-  fieldTypeInGeneratedSource?: string | {[srcLang: string]: string};
-  withTableAliasAs?: string; // Table alias escape sequence which may be used in value (default '$$').
-}
-
-export interface ParentSpec
-{
-  referenceName?: string | undefined;
-  customJoinCondition?: CustomJoinCondition;
-  tableJson: TableJsonSpec;
-  viaForeignKeyFields?: string[];
-}
-
-export interface ReferencedParentSpec extends ParentSpec
-{
-  referenceName: string;
-}
-
-export interface InlineParentSpec extends ParentSpec
-{
-  referenceName: undefined;
-}
-
-export interface ChildSpec
-{
-  collectionName: string;
-  tableJson: TableJsonSpec;
-  foreignKeyFields?: string[];
-  customJoinCondition?: CustomJoinCondition;
-  filter?: string;
-  unwrap?: boolean;
-  orderBy?: string;
-}
-
-export interface CustomJoinCondition
-{
-  equatedFields: FieldPair[];
-}
-
-export interface FieldPair
-{
-  childField: string;
-  parentPrimaryKeyField: string;
-}
-
-export interface RecordCondition
-{
-  sql: string;
-  paramNames?: string[];
-  withTableAliasAs?: string;
-}
-
-export interface SpecLocation
-{
-  queryName: string;
-  queryPart?: string;
-}
-
-export function addLocPart(specLoc: SpecLocation, addPart: string)
-{
-  return {
-    ...specLoc,
-    queryPart: !specLoc.queryPart ? addPart: `${specLoc.queryPart} / ${addPart}`
-  };
-}
-
-export class SpecError extends Error
-{
-  constructor(public specLocation: SpecLocation, public problem: string)
-  {
-    super(`SpecError in query '${specLocation.queryName}' at ${specLocation.queryPart || 'top level'}: ${problem}`);
-  }
-}
-
-export type PropertyNameDefault = "AS_IN_DB" | "CAMELCASE";
-
-export type ResultRepr = "MULTI_COLUMN_ROWS" | "JSON_OBJECT_ROWS" | "JSON_ARRAY_ROW";
-
+import {QuerySpec, TableJsonSpec, ReferencedParentSpec, InlineParentSpec}
+  from './query-spec-types.ts';
+export * from './query-spec-types.ts';
 
 export function getInlineParentSpecs(tableSpec: TableJsonSpec): InlineParentSpec[]
 {
@@ -146,3 +36,26 @@ function getParamNames(tableJsonSpec: TableJsonSpec): string[]
 
   return paramNames;
 }
+
+export function addLocPart(specLoc: SpecLocation, addPart: string)
+{
+  return {
+    ...specLoc,
+    queryPart: !specLoc.queryPart ? addPart: `${specLoc.queryPart} / ${addPart}`
+  };
+}
+
+export interface SpecLocation
+{
+  queryName: string;
+  queryPart?: string;
+}
+
+export class SpecError extends Error
+{
+  constructor(public specLocation: SpecLocation, public problem: string)
+  {
+    super(`SpecError in query '${specLocation.queryName}' at ${specLocation.queryPart || 'top level'}: ${problem}`);
+  }
+}
+
