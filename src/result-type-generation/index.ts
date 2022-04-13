d18dca265a9ff8ddd985859710f338a22aa3bb32
Stephen Harris <steveofar@hotmail.com>
2022-04-13 17:29:33 -0500

    Remove last vestiges of query-repr-sql-path.

--- src/result-type-generation/index.ts
+++ src/result-type-generation/index.ts
@@ -2,4 +2,3 @@ export * from './result-type-source-generator';
 export * from './result-type-spec-generator';
 export * from './result-type-specs';
 export * from './result-types-source';
-export * from './query-repr-sql-path';
