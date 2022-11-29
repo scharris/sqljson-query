03de86c74643d8bb5e3b3dd056d05ef33cda603d
Stephen Harris <steveofar@hotmail.com>
2022-11-29 10:12:57 -0600

    Switch to flake.nix and add direnv support.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "SQL/JSON Query";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs-16_x
+              nodePackages.typescript
+              openjdk17
+              maven
+              graphviz-nox
+            ];
+          };
+        }
+      );
+}
